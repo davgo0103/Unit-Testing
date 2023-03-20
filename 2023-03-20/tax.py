41ec063f242b7d4bedc0a4c5d925a0f924d56846
davgo0103 <davvgo@gmail.com>
2023-03-20 17:40:01 +0800

    Update

--- 2023-03-20/tax.py
+++ 2023-03-20/tax.py
@@ -1,28 +1,22 @@
-def leave(tax):
-    print(tax)
-    exit()
-
 def calc_tax(annual_income):
     annual_income -= 124000
     if(annual_income <= 92000):
         tax = 0
-        leave(tax)
+        return tax
     
 
     elif(annual_income > 0 and annual_income <= 560000): # 5%
         tax = annual_income * 0.05
-        leave(tax)
+        return tax
     elif(annual_income >= 560001 and annual_income <= 1260000): #12%
         tax = (annual_income - 560000) * 0.12 + 28000
-        leave(tax)
+        return tax
     elif(annual_income >= 1260001 and annual_income <= 2520000): #20%
-        tax = (annual_income - 1260000) * 0.12 + 179200
-        leave(tax)
+        tax = (annual_income - 1260000) * 0.20 + 179200
+        return tax
     elif(annual_income >= 2520001 and annual_income <= 4720000): #30%
-        tax = (annual_income - 2520000) * 0.2 + 229600
-        leave(tax)
+        tax = (annual_income - 2520000) * 0.30 + 229600
+        return tax
     elif(annual_income >= 4720001): # 40%
-        tax = (annual_income - 4720000) * 0.4 + 1645600
-        leave(tax)
-
-calc_tax()
+        tax = (annual_income - 4720000) * 0.40 + 1645600
+        return tax
--- 2023-03-20/test_tax.py
+++ 2023-03-20/test_tax.py
@@ -1,1 +1,21 @@
 import tax
+def test_5(): # 1 ~ 560000 = Annual_income * 0.05
+    val = 560000
+    result = tax.calc_tax(val)
+    assert result == 21800
+def test_12(): # 1 ~ 560000 = (Annual_income - 560000) * 0.12 + 28000
+    val = 560000
+    result = tax.calc_tax(val)
+    assert result == 21800
+def test_20(): # 1 ~ 560000 = (Annual_income - 1260000) * 0.20 + 179200
+    val = 560000
+    result = tax.calc_tax(val)
+    assert result == 21800
+def test_30(): # 1 ~ 560000 = (Annual_income - 2520000) * 0.30 + 229600
+    val = 560000
+    result = tax.calc_tax(val)
+    assert result == 21800
+def test_40(): # 1 ~ 560000 = (nnual_income - 4720000) * 0.40 + 1645600
+    val = 560000
+    result = tax.calc_tax(val)
+    assert result == 21800
