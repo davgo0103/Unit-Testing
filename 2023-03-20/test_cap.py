090e9949f8ab0416d160daee94d689f82a59662c
davgo0103 <davvgo@gmail.com>
2023-03-20 17:22:00 +0800

    Create

--- 2023-03-20/test_cap.py
+++ 2023-03-20/test_cap.py
@@ -7,5 +7,4 @@ def test_multiple_words():
     text = 'a veritable flock of ducks'
     result = cap.just_do_it(text)
     assert result == 'A Veritable Flock Of Ducks'
-if __name__ == '__main__':
-    main()
+
